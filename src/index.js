67409e50abae56dfc6681e3e93b56bdaeec0b6ee
Chris Olsen <chrisevjenolsen@gmail.com>
2023-04-13 13:20:59 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import "./styles.css"
+
+//TODOOS Project
+/*
+Functionality:
+-- The Basics --
+- Add reminders with a description, date and time -> Saved to localStorage
+
+- Reminders are seperated by todays things, later day things are ordered by priority
+
+- The reminders can also be given the TOPIC in journal it is associated with. 
+
+- Keep a virtual journal. Each note is automatically dated to the date and time it is written
+
+- Journal can be seperated by topics. So if you have several things you are studying, you can
+    journal stuff that has to do with that topic and keep them seperate. 
+
+-- Layout --
+- Header with logo top left, your name on the top right -> name is stored in localStorage
+
+-Side bar (collapses for mobile devies) 
+    - Reminders, Journal, Settings (all with icons)
+    - Settings will contain styling options for font, and color scheme
+
+- Reminders:
+    -Overdue --> All overdue things ordered by priority
+    - Today at top, and then a checklist
+        - Also ordered by priority
+    - Other:
+        - Top Priority RED - and then a checklist
+        - middle priority yellow - and then a checklist
+        - low priority green - and then a checklist
+- Journal:
+    - Grid, from most recent to oldest, or filtered by project and other
+    - Each journal input should have a title, and date by the side, and up to 
+      90 chars or so of the note itself visible
+*/
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "TODOOS",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+}
